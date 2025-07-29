a910a8f2109988d2e559f254cdfa1de4c2841e79
woojin-tionnet <dldnwls0115@tionnet.com>
2025-07-29 14:48:12 +0900

    Version -  versionCode 2로 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.woojin.recipick"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
