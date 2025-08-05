ea243d46970338b8894d8d4a1339f12fc3b16f9c
woojin-tionnet <dldnwls0115@tionnet.com>
2025-08-05 13:42:30 +0900

    Version - 알파 베포
    1. versionName 1.1, versionCode 4로 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.woojin.recipick"
         minSdk = 28
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
