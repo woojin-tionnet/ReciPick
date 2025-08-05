e63afe201e06b48a8345bab13fd406568843b4d4
woojin-tionnet <dldnwls0115@tionnet.com>
2025-08-05 13:17:55 +0900

    Feat - 애드몹 기본 초기화

--- app/src/main/java/com/woojin/recipick/application/RecipeApplication.kt
+++ app/src/main/java/com/woojin/recipick/application/RecipeApplication.kt
@@ -1,8 +1,18 @@
 package com.woojin.recipick.application
 
 import android.app.Application
+import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @HiltAndroidApp
 class RecipeApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        CoroutineScope(Dispatchers.IO).launch {
+            MobileAds.initialize(this@RecipeApplication)
+        }
+    }
 }
